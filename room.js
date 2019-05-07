e85a5debfddfe62ddf1cb050e78222c4dde21b86
mebaysan <menesbaysan@gmail.com>
2019-05-07 14:06:52 +0300

    room improve

--- room.js
+++ room.js
@@ -11,9 +11,21 @@ io.on('connection', (socket) => {
     console.log('Sunucuya biri bağlandı!');
     socket.on('joinRoom', (data) => {
         socket.join(data.name, () => { // 1.parametre ahangi odaya katılmak istediğimizi söyler. join methodu oda varsa katılır yoksa odayı oluşturur
-            let count = io.sockets.adapter.rooms[data.name].length; // data.name adlı odadaki datalara erişmek istiyorum. hangi datalar? length adlı data
-            io.to(data.name).emit('new join',{count}); // data.name burada oda adıdır. socket.to diyerek mesaj gönderebiliriz.
-            socket.emit('log',{message:'odaya girdiniz'});
+            // let count = io.sockets.adapter.rooms[data.name].length; // data.name adlı odadaki datalara erişmek istiyorum. hangi datalar? length adlı data
+            io.to(data.name).emit('new join', { count: getOnlineCount(io, data) }); // data.name burada oda adıdır. socket.to diyerek mesaj gönderebiliriz.
+            socket.emit('log', { message: 'odaya girdiniz' });
         }); // socket.to diyerek aktif tcp bağlantısı olan hişi hariç herkese gönderecek ama io.to dersek aktif tcp olan da dahil herkese gönderecek
     });
+
+    socket.on('leaveRoom', (data) => {
+        socket.leave(data.name, () => {
+            io.to(data.name).emit('leavedRoom', { count: getOnlineCount(io, data) });
+            socket.emit('socket.leaved', { message: 'Odadan ayrıldınız' });
+        });
+    });
 });
+
+const getOnlineCount = (io, data) => {
+    const room = io.sockets.adapter.rooms[data.name];
+    return room ? room.length : 0;
+}  // online sayısını bulacak bir fonk yazdık
